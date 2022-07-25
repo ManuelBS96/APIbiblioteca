07f7cd80ad62997f1742e4d12c87f9d6c56dcbd8
ManuelBS96 <manuelbns60@gmail.com>
2022-07-24 21:35:38 -0500

    Agregar archivos de proyecto.

---
+++ APIbiblioteca/Entidades/Autores.cs
@@ -0,0 +1,10 @@
+﻿namespace APIbiblioteca.Entidades
+{
+    public class Autores
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
---
+++ APIbiblioteca/Entidades/Editorial.cs
@@ -0,0 +1,9 @@
+﻿namespace APIbiblioteca.Entidades
+{
+    public class Editorial
+    {
+        public string IdEditorial { get; set; }
+        public string NameEditorial { get; set; }    
+
+    }
+}
